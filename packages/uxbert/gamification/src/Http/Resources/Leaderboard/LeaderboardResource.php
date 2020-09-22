a01b9440483697d30ada6259f32c27f5ec675c98
ahmedelroba <ahmed.elroba@uxbert.com>
2020-09-22 18:51:36 +0300

    new updatess

--- packages/uxbert/gamification/src/Http/Resources/Leaderboard/LeaderboardResource.php
+++ packages/uxbert/gamification/src/Http/Resources/Leaderboard/LeaderboardResource.php
@@ -27,6 +27,7 @@ class LeaderboardResource extends JsonResource
         $rewardsJsonArray = array();
         if(!empty($this->rewards)) {
             $rewards = json_decode($this->rewards);
+            return $rewards;
             foreach($rewards as $value){
                 $reward = Reward::where("_id", '=', $value->reward_id)->first();
                 $rewardsJsonArray[] = array('reward' => new RewardResource($reward), 'rank' => $value->rank);
