c7e400af62f0937b5e64393a60691374ba8b3fc6
ahmedelroba <ahmed.elroba@uxbert.com>
2020-09-24 11:08:32 +0300

    new updates

--- packages/uxbert/gamification/src/Http/Requests/API/Sponsor/CreateSponsorRequest.php
+++ packages/uxbert/gamification/src/Http/Requests/API/Sponsor/CreateSponsorRequest.php
@@ -29,7 +29,7 @@ class CreateSponsorRequest extends FormRequest
             'name'          => 'required|max:254',
             'logo'          => 'required|file',
             'description'   => 'required',
-            'status'        => 'required|boolean',
+            'status'        => 'boolean',
         ];
     }
 }
