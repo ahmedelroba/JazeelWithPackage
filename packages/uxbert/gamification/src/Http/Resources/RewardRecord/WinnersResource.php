68ed799ba8368e0500f6a41db5eb697e7baa86f2
ahmedelroba <ahmed.elroba@uxbert.com>
2020-09-27 19:42:40 +0300

    new updates

--- packages/uxbert/gamification/src/Http/Resources/RewardRecord/WinnersResource.php
+++ packages/uxbert/gamification/src/Http/Resources/RewardRecord/WinnersResource.php
@@ -21,7 +21,7 @@ class WinnersResource extends JsonResource
         $userObject = Client_User::find($this->user_id);
         $user = new UserResource($userObject);
         return [
-            'user'                  => $userObject,
+            'user'                  => 'ss',
             'given_to'              => $this->given_to,
             'leaderboard_rank'      => $this->leaderboard_rank,
             'status'                => $this->status,
